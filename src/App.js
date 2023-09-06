935931257590c6816393fe34b70723cf3e3db2bd
hsperez6 <hsperez@gmail.com>
2023-09-06 15:23:16 -0700

    Finished gif-search

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
 import React, { useEffect, useState } from "react";
-import SearchForm from './Components/SearchForm';
+import axios from "axios";
+
+import SearchForm from "./Components/SearchForm";
 import GifList from "./Components/GifList";
 
 function App() {
-
-  const [gifs, setGifs] = useState([])
+  const [gifs, setGifs] = useState([]);
+  const [query, setQuery] = useState("cats");
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    fetch("https://api.giphy.com/v1/gifs/trending?api_key=9RKp8rV7bQU2cdI0gWmQneqgJPqU3Ydh&limit=24&rating=g")
-      .then(response => response.json())
-      .then(responseData => setGifs(responseData.data))
-      .catch(error => console.log("Error fetching and parsing data", error));
-  }, []);
+    setLoading(true);
+    let activeFetch = true;
+
+    axios
+      .get(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=9RKp8rV7bQU2cdI0gWmQneqgJPqU3Ydh&limit=24&rating=g`)
+      .then(response => {
+        // handle success
+        if (activeFetch) {
+          setGifs(response.data.data);
+          setLoading(false);
+        }
+      })
+      .catch(function (error) {
+        // handle error
+        console.log("Error fetching and parsing data", error);
+      });
+
+      return () => {activeFetch = false};
+
+
+  }, [query]);
+
+  const handleQueryChange = searchText => {
+    setQuery(searchText)
+  };
 
   return (
     <div>
       <div className="main-header">
         <div className="inner">
           <h1 className="main-title">GifSearch</h1>
-          <SearchForm />
+          <SearchForm changeQuery={handleQueryChange}/>
         </div>
       </div>
       <div className="main-content">
-        <GifList />
+        {
+          (loading)
+          ? <p>Loading...</p>
+          : <GifList data={gifs}/>
+        }
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 
-const SearchForm = props => {
-  const [searchText, setSearchText] = useState('');
+function SearchForm ({changeQuery}) {
+  const searchText = useRef(null);
+
 
   const handleSubmit = e => {
     e.preventDefault();
-    e.currentTarget.reset()
+    changeQuery(searchText.current.value);
+    e.currentTarget.reset();
   }
 
   return (
     <form className="search-form" onSubmit={e => handleSubmit(e)} >
       <label className="is-hidden" htmlFor="search">Search</label>
       <input type="search" 
-             onChange={e => setSearchText(e.target.value)}
+             ref={searchText}
              name="search" 
              placeholder="Search..." />
       <button type="submit" id="submit" className="search-button"><i className="material-icons icn-search">search</i></button>
